d983d1fc2c6b234dc6c9486c049817cc9f82ba49
behappyleee <72903623+behappyleee@users.noreply.github.com>
2024-01-17 23:43:42 +0900

    project commit

---
+++ src/main/kotlin/com/example/parsething/ParseThingApplication.kt
@@ -0,0 +1,11 @@
+package com.example.parsething
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ParseThingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ParseThingApplication>(*args)
+}
